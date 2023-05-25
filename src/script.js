463a920a5ef19ad74a20400c0a2ce74bc4084e4c
vigopa1975 <victorgonzalespages@gmail.com>
2023-05-25 18:29:20 +0200

    Logica cambiara color fondo azul

--- src/script.js
+++ src/script.js
@@ -2,5 +2,14 @@ const boton_1 = document.getElementById('cambiarColor_1')
 
 boton_1.addEventListener('click', () => {
   const body = document.querySelector('body')
-  body.style.backgroundColor = 'yellow'
+  body.style.backgroundColor = 'yellow
+  ```js
+  const boton_2 = document.getElementById('cambiarColor_2')
+  
+  boton_2.addEventListener('click', () => {
+    const body = document.querySelector('body')
+    body.style.backgroundColor = 'blue'
+  })
+  ```
+
 })
